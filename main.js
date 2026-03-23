d9e3cc722f1843ee916f55025d19f2f082b8d7ab
Jessica Heusler <Jessica.Heusler@gmail.com>
2026-03-22 23:46:29 -0500

    finished converting character arrays practice

---
+++ main.js
@@ -0,0 +1,19 @@
+let message1 = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+let decoded1 = message1.split('').reverse().join('');
+console.log(decoded1);
+
+let message2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+let decoded2 = message2.split('').reverse().join('');
+console.log(decoded2);
+
+let message3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+let decoded3 = message3.split('').reverse().join('');
+console.log(decoded3);
+
+let message4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+let decoded4 = message4.split('').reverse().join('');
+console.log(decoded4);
+
+let myMessage = "Keep going, you’ve got this!";
+let reversedMessage = myMessage.split('').reverse().join('');
+console.log(reversedMessage);
